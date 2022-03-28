e19ce6c035f420d5b98e30c7ebf8865c4c329ab3
Matt Eilar <meilar@gmail.com>
2022-03-27 18:31:36 -0700

    Refactor: remove unneeded namespace declarations

--- Gruyere/Controllers/AccountController.cs
+++ Gruyere/Controllers/AccountController.cs
@@ -1,14 +1,8 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Gruyere.Models;
 using Gruyere.ViewModels;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
-using System.Security.Claims;
 
 namespace Gruyere.Controllers
 {
